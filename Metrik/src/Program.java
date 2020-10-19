bf4782daeefd9fcdb0401d2e3776b2d5be82873c
Hoghe <72462696+Hoghe@users.noreply.github.com>
2020-10-19 12:21:11 +0200

    Fertige Variante 1.1.1

--- Metrik/src/Program.java
+++ Metrik/src/Program.java
@@ -3,7 +3,7 @@ public class Program {
 
 
 	public static void main(String[] args) {
-		MainWindow  Fenster = new MainWindow();
+		MainWindow  Fenster = new MainWindow(400, 500);
 		Fenster.openWindow();
 
 	}
