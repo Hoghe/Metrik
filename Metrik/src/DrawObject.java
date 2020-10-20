e76cb88591a6a3ccac713d4088257c6f99144aa5
Mike Rohloff <u34825@hs-harz.de>
2020-10-20 11:10:09 +0200

    Fertige Version 1.2.1

--- Metrik/src/DrawObject.java
+++ Metrik/src/DrawObject.java
@@ -10,8 +10,11 @@ public class DrawObject extends Canvas implements MouseListener{
 	private int _breite;
 	private int _hoehe;
 	private double _radius;
-
-	
+	private String _lineLength = "";
+	private double _posX1 = 0;
+	private double _posX2 = 0;
+	private double _posY1 = 0;
+	private double _posY2 = 0;
 	
 	public DrawObject(int _breite, int _hoehe, TextField _x1, TextField _y1, TextField _x2, TextField _y2, TextField _tfr) {
 		this._x1 = _x1;
@@ -33,8 +36,12 @@ public class DrawObject extends Canvas implements MouseListener{
 		Graphics2D g2d = (Graphics2D) g;
 		switch(auswahl) {
 		case ZEICHNEN:
-			Ellipse2D kreis = new Ellipse2D.Double(_x, _y, _radius, _radius);
+			Ellipse2D kreis = new Ellipse2D.Double(_x, _y, _radius*2, _radius*2);
 			g2d.draw(kreis);
+			Font hypo = new Font("Arial", Font.BOLD, 12);
+			g2d.setFont(hypo);
+			g2d.drawString( _lineLength, _breite/100*25, 15 );
+			_lineLength = "";			
 			break;
 		case LOESCHEN:
 			g2d.clearRect(0, 0, _breite, _hoehe);
@@ -45,6 +52,11 @@ public class DrawObject extends Canvas implements MouseListener{
 		
 		String text = _tfr.getText();
 		this._radius = 0.0;
+		
+		double a = 0.0;
+		double b = 0.0;
+		double c = 0.0;
+		
 	    try
 	    {
 	      double zahl = Double.parseDouble(text);
@@ -54,48 +66,54 @@ public class DrawObject extends Canvas implements MouseListener{
 	    {
 	    	_tfr.setText("Bitte Zahl eingeben");
 	    }
-				
+		
 		_x = e.getX();
 		_y = e.getY();
 
 		if(_count == 0 && _radius>0.0) {
 			paint(getGraphics(), zeichenmodus.ZEICHNEN);
-			_x1.setText("x= "+(_x+_radius));
-			_y1.setText("y= "+(_y+_radius));
+			_x1.setText(""+(_x+_radius));
+			_y1.setText(""+(_y+_radius));
+			_posX1 = _x + _radius;
+			_posY1 = _y + _radius;
 			_count++;
 		} else if (_count==1 && _radius>0.0) {
+			_x2.setText(""+(_x+_radius));
+			_y2.setText(""+(_y+_radius));
+			_posX2 = _x + _radius;
+			_posY2 = _y + _radius;
+			
+			b = _posX2 - _posX1;
+			a = _posY2 - _posY1;
+			c = Math.round( 100.0 * Math.sqrt( a*a + b*b ) ) / 100.0;
+			_lineLength = _lineLength + "Länge der Verbindungslinie: " + c;
+			
 			paint(getGraphics(), zeichenmodus.ZEICHNEN);
-			_x2.setText("x= "+(_x+_radius));
-			_y2.setText("y= "+(_y+_radius));
 			_count++;		
 		} else {
 			paint(getGraphics(), zeichenmodus.LOESCHEN);
 			_count=0;
-
 		}
+		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Automatisch generierter Methodenstub
 		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Automatisch generierter Methodenstub
 		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Automatisch generierter Methodenstub
 		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Automatisch generierter Methodenstub
 		
 	}
 	
